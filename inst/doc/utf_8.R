814e27c7af7eff3ba363a8eacfc5e24961ee935c
Bruno André Rodrigues coelho <bruno@brodrigues.co>
2019-04-30 09:30:03 +0000

    version 1.0.0

---
+++ inst/doc/utf_8.R
@@ -0,0 +1,14 @@
+## ---- include = FALSE----------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library(michelRodange)
+#  
+#  data(renert)
+#  
+#  renert$line <- stringi::stri_unescape_unicode(line)
+#  renert$gesank <- stringi::stri_unescape_unicode(gesank)
+
